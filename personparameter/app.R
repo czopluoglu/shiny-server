7238f7d89170ce4bbe536c9c2e1750ee399fdb55
czopluoglu <cen.zop@gmail.com>
2020-02-13 17:02:58 -0500

    personparameter app update

--- personparameter/app.R
+++ personparameter/app.R
@@ -19,20 +19,22 @@ ui <- fluidPage(
                
                sliderInput("n", label = h4("Select the Number of Items"), min = 5, max = 10, value = 10),
                
-               actionBttn("GenItem", 
-                          label = "Generate the Item Parameters",
-                          icon=icon("play"), 
-                          style="simple",
-                          color="primary",
-                          size = "sm"),
+              # actionBttn("GenItem", 
+              #            label = "Generate the Item Parameters",
+              #            icon=icon("play"), 
+              #            style="simple",
+              #            color="primary",
+              #            size = "sm"),
                
-               checkboxInput("guessing", label = "Fix the guessing parameters to 0", value = TRUE),
-               checkboxInput("slipping", label = "Fix the slipping parameters to 1", value = TRUE),
+               checkboxInput("rasch", label = "Fix the discrimination parameters to 1.7", value = FALSE),
+               checkboxInput("disc", label = "Fix the discrimination parameters to be same", value = FALSE),
+               checkboxInput("guessing", label = "Fix the guessing parameters to 0", value = FALSE),
+               checkboxInput("slipping", label = "Fix the slipping parameters to 1", value = FALSE),
                
                mainPanel(tableOutput("ipar")),
                
                actionBttn("Genresp", 
-                          label = "Enter the Response Vector",
+                          label = "Generate the Response Vector",
                           icon=icon("play"), 
                           style="simple",
                           color="primary",
@@ -41,27 +43,28 @@ ui <- fluidPage(
               conditionalPanel(
                 
                 condition = "input.Genresp==1",
-                textInput('resp', 'Enter a response vector'),
+                textInput('resp', 'You can manipulate the response vector manuall below'),
                 helpText("*The length of response vector should be same as the number of items you selected."),
                 helpText("**The response vector should be comma separated."),
                 helpText("*** The response vector should include only 0 (incorrect response) or 1 (correct response).")
                 
                 
               ) 
-               
- 
               
         ),
         
         column(8,
                mainPanel(
                    tabsetPanel(
-                       tabPanel("Guesstimate",
+                       tabPanel("Guesstimate", align='center',
+                                
+                                br(),
+                                br(),
+                                br(),
                                 
                                 conditionalPanel(
                                   condition = "input.Genresp==1",
                                   tableOutput("resp2")),
-                                
                                 
                                 conditionalPanel(
                                   condition = "input.Genresp==1",
@@ -90,17 +93,23 @@ server <- function(input, output,session) {
     
     tab[,2] = rlnorm(input$n,0,.5)
     tab[,3] = rnorm(input$n,0,1)
+    tab[,4] = runif(input$n,0,.25)
+    tab[,5] = runif(input$n,0.9,1)
     
-    if(input$guessing==FALSE){
-      tab[,4] = runif(input$n,0,.25)
-    } else {
+    if(input$guessing==TRUE){
       tab[,4]=0
     }
     
-    if(input$slipping==FALSE){
-      tab[,5] = runif(input$n,0.9,1)
-    } else {
+    if(input$slipping==TRUE){
       tab[,5]=1
+    }
+    
+    if(input$rasch==TRUE){
+      tab[,2]=1.7
+    }
+    
+    if(input$disc==TRUE){
+      tab[,2]=rlnorm(1,0,.5)
     }
     
     tab 
@@ -110,8 +119,8 @@ server <- function(input, output,session) {
   
   ####################################################
 
-  tabb.table <- eventReactive(input$GenItem, {
-
+  tabb.table <- reactive({
+    
     tabb()
     
    })
@@ -120,11 +129,32 @@ server <- function(input, output,session) {
    output$ipar  <- renderTable({tabb.table()})
 
   ####################################################
-
+   
   
   observe({
+    
       x <- input$n
-      updateTextInput(session, "resp", value = c(1,rep(0,x-1)))
+      ipars <- tabb()
+      
+      th = rnorm(1,0,1)
+      
+      p <- function(theta,a,b,c,d){
+        num = exp(a*(theta-b))
+        c+((d-c)*(num/(1+num)))
+      }
+      
+      rvec = c()
+      
+      for(j in 1:input$n){
+        a=ipars[j,2]
+        b=ipars[j,3]
+        c=ipars[j,4]
+        d=ipars[j,5]
+        
+        rvec[j] = rbinom(1,1,p(theta=th,a=ipars[j,2],b=ipars[j,3],c=ipars[j,4],d=ipars[j,5]))
+      }
+    
+      updateTextInput(session, "resp", value = rvec)
   })
     
   respvec <- eventReactive(input$resp, {
