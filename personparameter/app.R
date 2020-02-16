faadcce620c9f55d86f992de25a1cd9a631b0533
czopluoglu <cen.zop@gmail.com>
2020-02-15 22:46:48 -0500

    personparameter app is getting very cool!

--- personparameter/app.R
+++ personparameter/app.R
@@ -2,9 +2,122 @@
 library(shiny)
 library(shinyWidgets)
 
+
+##########################################################################################
+loglikelihood <- function(ipar,resp,t){
+  
+  amat <- matrix(ipar[,2],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  bmat <- matrix(ipar[,3],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  cmat <- matrix(ipar[,4],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  dmat <- matrix(ipar[,5],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  tmat <- matrix(t,nrow=length(t),ncol=nrow(ipar),byrow=FALSE)
+  num  = exp(amat*(tmat-bmat))
+  pmat = cmat + (dmat-cmat)*(num/(1+num))
+  r <- matrix(resp,nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  p = (pmat^r)*((1-pmat)^(1-r))
+  rowSums(log(p))
+}  
+
+l1 <- function(ipar,resp,t){
+  
+  amat <- matrix(ipar[,2],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  bmat <- matrix(ipar[,3],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  cmat <- matrix(ipar[,4],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  dmat <- matrix(ipar[,5],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  tmat <- matrix(t,nrow=length(t),ncol=nrow(ipar),byrow=FALSE)
+  x  = exp(amat*(bmat-tmat))
+  r <- matrix(resp,nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+
+  a=(-(amat*x*(dmat-cmat))*((r-cmat)*x+r-dmat))
+  b=(x+1)*((cmat-1)*x+dmat-1)*(cmat*x+dmat)
+  sum(a/b)
+} 
+
+# First derivative of 4PL with respect to theta
+# Latex equation code
+# -\dfrac{a\left(d-c\right)\mathrm{e}^{a\left(b-t\right)}\left(\left(r-c\right)\mathrm{e}^{a\left(b-t\right)}+r-d\right)}{\left(\mathrm{e}^{a\left(b-t\right)}+1\right)\left(\left(c-1\right)\mathrm{e}^{a\left(b-t\right)}+d-1\right)\left(c\mathrm{e}^{a\left(b-t\right)}+d\right)}
+
+l2 <- function(ipar,resp,t){
+  
+  amat <- matrix(ipar[,2],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  bmat <- matrix(ipar[,3],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  cmat <- matrix(ipar[,4],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  dmat <- matrix(ipar[,5],nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  tmat <- matrix(t,nrow=length(t),ncol=nrow(ipar),byrow=FALSE)
+  x  = exp(amat*(bmat-tmat))
+  r <- matrix(resp,nrow=length(t),ncol=nrow(ipar),byrow=TRUE)
+  
+  term1 = ((amat^2)*(dmat-cmat)*(r-cmat)*(x^2))/
+            ((x+1)*((cmat-1)*x+dmat-1)*(cmat*x+dmat))
+  
+  term2 = ((amat^2)*(dmat-cmat)*((r-cmat)*x+r-dmat)*(x^2))/
+            (((x+1)^2)*((cmat-1)*x+dmat-1)*(cmat*x+dmat))
+  
+  
+  term3 = ((amat^2)*(cmat-1)*(dmat-cmat)*((r-cmat)*x+r-dmat)*(x^2))/
+             ((x+1)*(((cmat-1)*x+dmat-1)^2)*(cmat*x+dmat))
+  
+  term4 = ((amat^2)*cmat*(dmat-cmat)*((r-cmat)*x+r-dmat)*(x^2))/
+             ((x+1)*((cmat-1)*x+dmat-1)*((cmat*x+dmat)^2))
+  
+  term5 = ((amat^2)*(dmat-cmat)*x*((r-cmat)*x+r-dmat))/
+             ((x+1)*((cmat-1)*x+dmat-1)*(cmat*x+dmat))
+  
+  sum(term1 - term2 - term3 - term4 + term5)
+} 
+
+# Second derivative of 4PL with respect to theta
+# Latex equation code
+# \dfrac{a^2\left(d-c\right)\left(r-c\right)\mathrm{e}^{2a\left(b-t\right)}}{\left(\mathrm{e}^{a\left(b-t\right)}+1\right)\left(\left(c-1\right)\mathrm{e}^{a\left(b-t\right)}+d-1\right)\left(c\mathrm{e}^{a\left(b-t\right)}+d\right)}-\dfrac{a^2\left(d-c\right)\left(\left(r-c\right)\mathrm{e}^{a\left(b-t\right)}+r-d\right)\mathrm{e}^{2a\left(b-t\right)}}{\left(\mathrm{e}^{a\left(b-t\right)}+1\right)^2\left(\left(c-1\right)\mathrm{e}^{a\left(b-t\right)}+d-1\right)\left(c\mathrm{e}^{a\left(b-t\right)}+d\right)}-\dfrac{a^2\left(c-1\right)\left(d-c\right)\left(\left(r-c\right)\mathrm{e}^{a\left(b-t\right)}+r-d\right)\mathrm{e}^{2a\left(b-t\right)}}{\left(\mathrm{e}^{a\left(b-t\right)}+1\right)\left(\left(c-1\right)\mathrm{e}^{a\left(b-t\right)}+d-1\right)^2\left(c\mathrm{e}^{a\left(b-t\right)}+d\right)}-\dfrac{a^2c\left(d-c\right)\left(\left(r-c\right)\mathrm{e}^{a\left(b-t\right)}+r-d\right)\mathrm{e}^{2a\left(b-t\right)}}{\left(\mathrm{e}^{a\left(b-t\right)}+1\right)\left(\left(c-1\right)\mathrm{e}^{a\left(b-t\right)}+d-1\right)\left(c\mathrm{e}^{a\left(b-t\right)}+d\right)^2}+\dfrac{a^2\left(d-c\right)\mathrm{e}^{a\left(b-t\right)}\left(\left(r-c\right)\mathrm{e}^{a\left(b-t\right)}+r-d\right)}{\left(\mathrm{e}^{a\left(b-t\right)}+1\right)\left(\left(c-1\right)\mathrm{e}^{a\left(b-t\right)}+d-1\right)\left(c\mathrm{e}^{a\left(b-t\right)}+d\right)}
+
+#require(numDeriv)
+
+
+#grad <- function(ipar,resp,t){
+#  
+#  temp = loglikelihood(ipar=ipar,resp=resp,t=t)
+#  temp2 = loglikelihood(ipar=ipar,resp=resp,t=t+.001)
+#  
+#  (temp2-temp)/(.001)
+#}
+
+#hess <- function(ipar,resp,t){
+#  
+#  temp  = grad(ipar=ipar,resp=resp,t=t)
+#  temp2 = grad(ipar=ipar,resp=resp,t=t+.001)
+#  
+#  (temp2-temp)/.001
+#}
+
+
+#th = 1
+#numDeriv::grad(func=loglikelihood,x = th,ipar=ipar,resp=resp)
+#grad(ipar=ipar,resp=resp,t=th)
+#l1(ipar=ipar,resp=resp,t=th)
+
+#hessian(func=loglikelihood,x = th,ipar=ipar,resp=resp)
+#hess(ipar=ipar,resp=resp,t=th)
+#l2(ipar=ipar,resp=resp,t=th)
+
+
 ##########################################################################################
 
 ui <- fluidPage(
+  
+  tags$head(tags$style("#nstep{color: black;
+                                 font-size: 14px;
+                                 font-weight: bold;
+                                 }"
+  )
+  ),
+  
+  tags$head(tags$style("#maxlogL{color: black;
+                                 font-size: 20px;
+                                 }"
+  )
+  ),
+  
+  
     
     setBackgroundColor(
         color = c("#ffffff", "#ffffff"),
@@ -75,38 +188,44 @@ ui <- fluidPage(
                        tabPanel("Guess MLE", align='center',
                                 
                                 br(),
+                                
+                                fluidRow(
+                                  column(12,align='center',
+                                         mainPanel(
+                                           tableOutput("resp2")
+                                           )
+                                  )
+                                ),
+                                
+                                fluidRow(
+                                  column(12,align='center',
+                                         mainPanel(
+                                           plotOutput('plot',width = "100%")
+                                           )
+                                  )
+                                ),
+                                
                                 br(),
-                                
-                                mainPanel(
-                                  tableOutput("resp2")
-                                  ),
-                                
-                                mainPanel(
-                                  plotOutput('plot')
-                                  ),
-                                
+                                br(),
                                 br(),
                                 br(),
                                 
                                 fluidRow(
                                   
                                   br(),
-                                  br(),
-                                  br(),
-                                  br(),
-                                  br(),
-                                  br(),
-                                  br(),
-                                  br(),
-                                  br(),
-                                  br(),
-
+                                
+                                  textOutput("maxlogL"),
                                   
-                                  column(4,
+                                  br(),
+                                  br(),
+                                  br(),
+                                  
+                                  column(3,
+                                         
                                          fluidRow(
                                                   numericInput("theta.est", 
                                                                label = "", 
-                                                               value = 0,
+                                                               value = NULL,
                                                                width='50%')
                                            ),
                                          
@@ -119,13 +238,18 @@ ui <- fluidPage(
                                            )
                                          ),
                                   
-                                  column(8,
-                                         'Output',
+                                  column(3,
                                            tableOutput("guess")
-                                         
-                                         
-                                         )
+                                         ),
                                   
+                                  column(3,
+                                         " "
+                                  ),
+                                  
+                                  column(3,
+                                         textOutput("nstep"),
+                                         tableOutput("guess2")
+                                  )
                                 )
                                 
                                 
@@ -254,53 +378,125 @@ server <- function(input, output,session) {
   
   ####################################################
   
+  values <- reactiveValues(df_data = data.frame(matrix(NA,nrow=1,ncol=3)),
+                           df_data2 = data.frame(matrix(NA,nrow=1,ncol=2)),
+                           step = 0,
+                           step2= 0
+                           )
+  
+                    
   guessm <- eventReactive(input$guess, {
-    if(input$theta.est==1){
-      matrix(1,nrow=5,ncol=2)
-    } else {
-      matrix(NA,nrow=5,ncol=2)
+    if(values$step==0){
+      colnames(values$df_data) <- c("Theta","Loglikelihood","Slope of Tangent Line")
     }
+    
+    values$step = values$step+1
+    
+    values$df_data[values$step,1]= input$theta.est
+    
+    ip <- tabb()
+    r  <- as.numeric(unlist(strsplit(input$resp,",")))
+    
+    values$df_data[values$step,2]= loglikelihood(ipar=ip,resp=r,t=input$theta.est)
+    
+    values$df_data[values$step,3]= l1(ipar=ip,resp=r,t=input$theta.est)
+    
+    
+    values$df_data
   })
   
-  output$guess <- renderTable({
-    guessmatrix = guessm()
-    guessmatrix
-    })
+  output$guess <- renderTable(digits=5,bordered=TRUE,striped=TRUE,{
+   guessm()
+  })
+  
+  ####################################################
+  
+  guessm2 <- eventReactive(input$guess, {
+    if(values$step2==0){
+      colnames(values$df_data2) <- c("Theta","Loglikelihood")
+    }
+    
+    values$step2 = values$step2+1
+    
+    values$df_data2[values$step2,1]= input$theta.est
+    
+    ip <- tabb()
+    r  <- as.numeric(unlist(strsplit(input$resp,",")))
+    
+    values$df_data2[values$step2,2]= loglikelihood(ipar=ip,resp=r,t=input$theta.est)
+    
+    values$df_data2
+  })
+  
+  output$guess2 <- renderTable(digits=3,{
+    guessm2()
+  })
     
   
   ####################################################
   
-  
-  output$plot <- renderPlot({
-  
+  my.maxlogL <- eventReactive(input$guess,{
     ipars <- tabb()
     responses2 <- as.numeric(unlist(strsplit(input$resp,",")))
     
-    p <- function(theta,a,b,c,d){
-      num = exp(a*(theta-b))
-      c+((d-c)*(num/(1+num)))
-    }
+    t <- seq(-4,4,.00005)
+    loglik <- loglikelihood(ipar=ipars,resp=responses2,t=t)
     
-    t <- seq(-4,4,.01)
+    maxL = round(max(loglik),5)
     
-    loglik <- c()
-    
-    for(i in 1:length(t)){
-      
-      pvec = c()
-      
-      for(j in 1:input$n){
-        pvec[j] = p(theta=t[i],a=ipars[j,2],b=ipars[j,3],c=ipars[j,4],d=ipars[j,5])  
-      }
-      
-      loglik[i] =   sum(log((responses2*pvec) + ((1-responses2)*(1-pvec))))
-      
-    }
-    
-    plot(t,loglik,type="l",lty=2,col="gray",xlab="Theta",ylab="Log-likelihood",
-         ylim=c(min(loglik)*1.1,max(loglik)*.9))
-    
+    paste0("The maximum value of loglikelihood function is ",maxL,
+           ". The slope of tangent line at the maximum point is 0. 
+           Find which theta value makes the loglikelihood maximum. 
+           Guess as many times as you like. Let the slope of tangent line guide you for your next guess.")
   })
+  
+  output$maxlogL <- renderText({
+    my.maxlogL()
+    })
+  
+  
+  output$nstep <- renderText({
+    paste0("Number of Guesses = ", values$step) 
+  })
+  
+  ####################################################
+  
+  myplot <- eventReactive(input$guess,{
+     
+    ip <- tabb()
+    r <- as.numeric(unlist(strsplit(input$resp,",")))
+    t <- seq(-4,4,.001)
+    
+    loglik <- loglikelihood(ipar=ip,resp=r,t=t)
+    
+    minn=min(loglik)
+    maxx=max(loglik)
+    rangex=maxx-minn
+    
+    plot(t,loglik,type="l",lty=2,xlab="Theta",ylab="Log-likelihood",
+         ylim=c(min(loglik)-rangex/4,max(loglik)+rangex/3))
+    
+    slope = l1(ipar=ip,resp=r,t=input$theta.est)
+    x0 = input$theta.est
+    y0 = loglikelihood(ipar=ip,resp=r,t=input$theta.est)
+    int = y0 - x0*slope
+    
+    points(x0,y0,pch=16,cex=1.4)
+    
+    segments(x0=input$theta.est,y0=min(loglik)-rangex/4,
+             x1=input$theta.est,y1=y0,
+             lty=2,col="gray")
+    
+    segments(x0=input$theta.est,y0=y0,
+             x1=t[1],y1=y0,
+             lty=2,col="gray")
+    
+    abline(coef=c(int,slope),lty=2,col="gray")
+  })
+  
+  output$plot <- renderPlot({
+    myplot()}
+    ,height = 500, width = 800)
   
   ####################################################
 }
