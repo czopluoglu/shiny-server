200259ad966aa544dfed0cebce35c8d657b6407b
Cengiz Zopluoglu <cen.zop@gmail.com>
2021-10-20 18:00:17 -0700

    ridge regression demo

---
+++ ridge/app.R
@@ -0,0 +1,85 @@
+
+setwd("B:/shinyserver/shiny-server/ridge")
+
+#setwd("/srv/shiny-server/deceased_turkey/data/")
+
+d <- read.csv('readability_feat_sub.csv')
+
+require(shiny)
+require(plotly)
+require(tidyr)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Regularized Regression with Ridge Penalty"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            numericInput("lambda", 
+                         label = "Lambda", 
+                         value = 0,
+                         width='50%'),
+            textOutput("ssr"),
+            textOutput("b0"),
+            textOutput("b1")
+            ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+          plotlyOutput('plot',width = '100%',height='100%')
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  myplot <- eventReactive(input$lambda,{
+
+      grid    <- expand.grid(b0=seq(-10,10,.1),b1=seq(-5,5,.01))           
+      grid$SSR <- NA
+      
+      B1    <- matrix(grid$b1,ncol=20,nrow=nrow(grid),byrow=FALSE)
+      B0    <- matrix(grid$b0,ncol=20,nrow=nrow(grid),byrow=FALSE)
+      X     <- matrix(d$mean.wl,ncol=20,nrow=nrow(grid),byrow=TRUE)
+      Y_hat <- B0 + X*B1
+      Y     <- matrix(d$target,ncol=20,nrow=nrow(grid),byrow=TRUE)
+      P     <- input$lambda*(grid$b0^2 + grid$b1^2)
+      grid$SSR <- rowSums((Y - Y_hat)^2) + P
+
+      pl <- plot_ly(grid, x = ~b0, y = ~b1, z = ~SSR, 
+              marker = list(color = ~SSR,
+                            showscale = TRUE,
+                            cmin=min(grid$SSR)*5,
+                            cmax=min(grid$SSR),cauto=F),
+              width=600,height=600) %>% 
+        add_markers()
+      
+      return(list(pl=pl,
+                  SSR = grid[which.min(grid$SSR),]$SSR,
+                  b0  = grid[which.min(grid$SSR),]$b0,
+                  b1  = grid[which.min(grid$SSR),]$b1))
+    })
+  
+  output$plot <- renderPlotly({
+    myplot()$pl
+  })
+  
+  output$ssr <- renderText({
+    paste0("Minimum SSR = ",round(myplot()$SSR,2)) 
+  })
+  
+  output$b0 <- renderText({
+    paste0("Estimated B0 = ",round(myplot()$b0,2)) 
+  })
+  
+  output$b1 <- renderText({
+    paste0("Estimated B1 = ",round(myplot()$b1,2)) 
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
